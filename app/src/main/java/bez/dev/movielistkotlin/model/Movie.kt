cf1bc25889747c1f0669e51b0d1a7014eb03819f
Betz <silverbez@gmail.com>
2020-01-29 10:03:18 +0200

    importing Room stuff

--- app/src/main/java/bez/dev/movielistkotlin/model/Movie.kt
+++ app/src/main/java/bez/dev/movielistkotlin/model/Movie.kt
@@ -1,14 +1,46 @@
 package bez.dev.movielistkotlin.model
 
+import android.os.Parcel
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
-@Parcelize
+@Entity(tableName = "movie_table")
 data class Movie(
-    val title: String,
+    @PrimaryKey val title: String,
     val image: String,
     val rating: Double,
     val releaseYear: Int,
     val genre: List<String>
-) : Parcelable
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readDouble(),
+        parcel.readInt(),
+        parcel.createStringArrayList()!!
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(title)
+        parcel.writeString(image)
+        parcel.writeDouble(rating)
+        parcel.writeInt(releaseYear)
+        parcel.writeStringList(genre)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Movie> {
+        override fun createFromParcel(parcel: Parcel): Movie {
+            return Movie(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Movie?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
 
