7593e441f88e764236d4a77b0e3d9f6ae418ab8f
Betz <silverbez@gmail.com>
2020-02-05 13:40:05 +0200

    added RxJava2CallAdapterFactory to retrofit, clean code and quick data loading!

--- app/src/main/java/bez/dev/movielistkotlin/model/MovieNetwork.kt
+++ app/src/main/java/bez/dev/movielistkotlin/model/MovieNetwork.kt
@@ -1,10 +1,8 @@
 package bez.dev.movielistkotlin.model
 
 import io.reactivex.Single
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
@@ -14,33 +12,20 @@ class MovieNetwork : IDataSource {
 
     private val retrofitMovieInstance = Retrofit.Builder().baseUrl(moviesBaseUrl)
         .addConverterFactory(GsonConverterFactory.create())
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
         .build()
 
-    private val movieCall = retrofitMovieInstance.create(RequestInterface::class.java).fetchJsonData()
+    private val movieCall = retrofitMovieInstance.create(RequestInterface::class.java)
+
 
     override fun fetchMoviesData(): Single<List<Movie>> {
-        return Single.create{ observer ->
-
-            movieCall?.enqueue(object : Callback<List<Movie>>{
-                override fun onResponse(call: Call<List<Movie>>, response: Response<List<Movie>>) {
-                    val movieList = response.body()
-                    if (movieList != null){
-                        observer.onSuccess(movieList)
-                    }
-                }
-
-                override fun onFailure(call: Call<List<Movie>>, t: Throwable) {
-                    observer.onError(t)
-                }
-
-            })
-        }
+        return movieCall.fetchJsonData()
     }
 
 
     interface RequestInterface {
         @GET("json/movies.json")
-        fun fetchJsonData(): Call<List<Movie>>?
+        fun fetchJsonData(): Single<List<Movie>>
     }
 
 
